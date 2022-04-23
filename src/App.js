685f09714339b26b6b5a020bde80c8941d4d9744
saeedhassansolangi <saeedhassansolangi@gmail.com>
2022-04-23 12:26:43 +0500

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Page2 } from './pages/Page2';
 import { Page3 } from './pages/Page3';
@@ -8,16 +8,16 @@ import { Result } from './pages/Result';
 
 function App() {
   return (
-    <div className="App">
-      <HashRouter>
+    <BrowserRouter>
+      <div className="App">
         <Routes>
           <Route path="/" element={<Home page="Home" />} />
           <Route path="page2" element={<Page2 page={' 2'} />} />
           <Route path="page3" element={<Page3 page={'Page 3'} />} />
           <Route path="result" element={<Result page={'Result'} />} />
         </Routes>
-      </HashRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
