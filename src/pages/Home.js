34f8ac3775dd2617382e42e29c15671c0bb794c6
saeedhassansolangi <saeedhassansolangi@gmail.com>
2022-04-24 07:14:58 +0500

    dynamic inputs completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export const Home = function ({ page }) {
   const [emailErrorMsg, setEmailErrorMsg] = React.useState('');
@@ -7,29 +8,77 @@ export const Home = function ({ page }) {
     window.localStorage.getItem('country')
   );
   const navigate = useNavigate();
+  const [serials, setSerials] = useState([]);
+  const [formValues, setFormValues] = useState([
+    { serialNumber: '', reason: '' },
+  ]);
 
-  const handleFormEmail = function (event) {
+  useEffect(() => {
+    axios
+      .get(`/apis/correctserial.json`)
+      .then((res) => {
+        const { data } = res.data;
+        setSerials(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  let handleChange = (i, e) => {
+    let newFormValues = [...formValues];
+    newFormValues[i][e.target.name] = e.target.value;
+    setFormValues(newFormValues);
+  };
+
+  let addFormFields = () => {
+    setFormValues([...formValues, { serialNumber: '', reason: '' }]);
+  };
+
+  let removeFormFields = (i) => {
+    let newFormValues = [...formValues];
+    newFormValues.splice(i, 1);
+    setFormValues(newFormValues);
+  };
+
+  const handleFormEmail = async function (event) {
     event.preventDefault();
+
+    console.log(serials);
 
     const {
       email: { value: emailAddress },
     } = event.target.elements;
 
     if (emailAddress) {
-      alert(`
-        here is the email : ${emailAddress}
-    `);
+      const email = emailAddress.toLowerCase();
 
-      // if the email address is in the database than redirect the user to the 'page3'
+      const res = await axios.get(`/apis/CorrectEmail.json`);
+
+      if (res.data.email !== email) {
+        setEmailErrorMsg('Not Found');
+        return;
+      }
+
+      alert(JSON.stringify(formValues));
+
       navigate('/page3');
     } else {
       setEmailErrorMsg('Please enter your email address');
     }
   };
   return (
-    <div className="home-page">
+    <div
+      className="home-page"
+      style={{
+        height: '100vh',
+        overflowY: 'scroll',
+        marginTop: '10px',
+        scrollBehavior: 'smooth',
+      }}
+    >
       {country ? (
-        <div>
+        <>
           <h2>
             Welcome!, to <span className="page">{page}</span>
           </h2>
@@ -49,40 +98,62 @@ export const Home = function ({ page }) {
               </p>
             </div>
 
-            <div className="reason-to-visit">
-              <div className="form-group">
-                <label htmlFor="reason">Reason For Visit</label>
-                <input
-                  type="reason"
-                  name="reason"
-                  id="reason"
-                  placeholder="Enter your reason..."
-                />
+            <div className="serial-numbers">
+              <div className="serials">
+                <h3>Serial Numbers </h3>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={addFormFields}
+                >
+                  +
+                </button>
               </div>
+              {formValues.map((formValue, i) => (
+                <div className="serial-number" key={i}>
+                  <div className="form-group">
+                    <label htmlFor="serialNumber">Serial Number</label>
 
-              <div className="form-group">
-                <label htmlFor="serial">Select Serial</label>
+                    <select
+                      name="serialNumber"
+                      id="serialNumber"
+                      onChange={(e) => handleChange(i, e)}
+                    >
+                      <option value="" disabled selected>
+                        Select Serial Number
+                      </option>
+                      {serials.map((serial, index) => (
+                        <option key={index} value={serial.serialnumber}>
+                          {serial.serialnumber}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
 
-                <select name="" id="">
-                  <option value="">1</option>
-                  <option value="">2</option>
-                  <option value="">3</option>
-                  <option value=""></option>
-                  <option value=""></option>
-                </select>
-              </div>
+                  <div className="form-group">
+                    <label htmlFor="reason">Reason For Visit</label>
+
+                    <select
+                      name="reason"
+                      id="reason"
+                      onChange={(e) => handleChange(i, e)}
+                    >
+                      <option value="" disabled selected>
+                        Select
+                      </option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                    </select>
+                  </div>
+
+                  <button type="button" onClick={() => removeFormFields(i)}>
+                    -
+                  </button>
+                </div>
+              ))}
             </div>
 
-            <div className="form-group">
-              <label htmlFor="country">Location</label>
-              <input
-                type="text"
-                name="country"
-                id="country"
-                readOnly
-                value={country}
-              />
-            </div>
             <button
               type="submit"
               className="form-btn"
@@ -91,9 +162,9 @@ export const Home = function ({ page }) {
               Submit
             </button>
           </form>
-        </div>
+        </>
       ) : (
-        (window.location.href = '/page2')
+        navigator('/page2')
       )}
     </div>
   );
